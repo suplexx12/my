9292dd176f5b39fee3b50e6da50c7a2cdcc31334
suplexx12 <46633873+suplexx12@users.noreply.github.com>
2019-01-12 23:27:26 +0300

    Update multi.sh

--- multi.sh
+++ multi.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
 #Setup Variables
 GREEN='\033[0;32m'
 YELLOW='\033[0;93m'
 RED='\033[0;31m'
 NC='\033[0m'
-
 #Checking OS
 if [[ $(lsb_release -d) != *16.04* ]]; then
   echo -e ${RED}"The operating system is not Ubuntu 16.04. You must be running on ubuntu 16.04."${NC}
   exit 1
 fi
-
 echo -e ${YELLOW}"Welcome to the Northern Automated Install, Durring this Process Please Hit Enter or Input What is Asked."${NC}
 echo
 echo -e ${YELLOW}"You Will See alot of code flashing across your screen, don't be alarmed it's supposed to do that. This process can take up to an hour and may appear to be stuck, but I can promise you it's not."${NC}
